fc5d4bb89625a3ed2138b686912dd8c8439a028e
balazsdukai <balazs.dukai@gmail.com>
2016-05-25 21:19:07 +0200

    distance on continental trajectory

--- compare_geom_geog.sql
+++ compare_geom_geog.sql
@@ -1,4 +1,4 @@
-
+-- create view for the contiental trajectory
 CREATE VIEW example_data.continental AS (
 SELECT * 
 FROM example_data.relocations
@@ -7,8 +7,8 @@ WHERE id='continental');
 -- GEOMETRY
 --select the point with the minimum Y value
 CREATE VIEW example_data.cont_ymin AS (
-SELECT geom 
-FROM example_data.continental, (
+SELECT rel.gid, rel.geom, rel.geog 
+FROM example_data.continental AS rel, (
     SELECT gid, st_x, st_y
     FROM (SELECT gid, 
                  st_x(geom), 
@@ -17,13 +17,13 @@ FROM example_data.continental, (
     WHERE xy.st_y = (SELECT min(st_y(geom))
                      FROM example_data.continental)
      ) AS cont
-WHERE example_data.continental.gid = cont.gid
+WHERE rel.gid = cont.gid
 );
 
 --select the point with the maximum Y value
 CREATE VIEW example_data.cont_ymax AS (
-SELECT geom
-FROM example_data.continental, (
+SELECT rel.gid, rel.geom, rel.geog
+FROM example_data.continental AS rel, (
     SELECT gid, st_x, st_y
     FROM (SELECT gid, 
                  st_x(geom), 
@@ -32,14 +32,25 @@ FROM example_data.continental, (
     WHERE xy.st_y = (SELECT max(st_y(geom))
                      FROM example_data.continental)
     ) AS cont
-WHERE example_data.continental.gid = cont.gid
+WHERE rel.gid = cont.gid
 );
 
 
 SELECT st_srid(cont_ymax.geom) 
 FROM example_data.cont_ymax;
 
-SELECT st_distance(cont_ymax.geom, cont_ymin.geom) 
+-- GEOMETRY distance (srid 3395)
+SELECT st_distance(st_transform(cont_ymax.geom, 3395), st_transform(cont_ymin.geom,3395)) 
 FROM example_data.cont_ymax, example_data.cont_ymin;
+--result: 4360838.681311944
+
+-- GEOGRAPHY distance
+SELECT st_distance(cont_ymax.geog, cont_ymin.geog) 
+FROM example_data.cont_ymax, example_data.cont_ymin;
+--result: 4124174.970921178
+
+SELECT st_distance(st_transform(a.geom, 3395), st_transform(b.geom,3395))
+FROM (select geom from example_data.continental where gid=14894) AS a,
+(select geom from example_data.continental where gid=14892) AS b;
 
 
