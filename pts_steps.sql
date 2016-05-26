a44c2f4e3f8d041c4f983484766fea6a6a4462ac
balazsdukai <balazs.dukai@gmail.com>
2016-05-26 08:26:31 +0200

    initial commit

---
+++ pts_steps.sql
@@ -0,0 +1,41 @@
+CREATE OR REPLACE FUNCTION fib (fib_for integer
+) RETURNS integer AS $$
+BEGIN
+IF fib_for < 2 THEN
+RETURN fib_for;
+END IF;
+RETURN fib(fib_for - 2) + fib(fib_for - 1);
+END;
+$$LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION fib_fast(
+     fib_for integer
+) RETURNS integer AS $$
+DECLARE
+    ret integer := 0;
+    nxt integer := 1;
+    tmp integer;
+BEGIN
+   FOR num IN 1..fib_for LOOP
+        tmp := ret;
+       ret := nxt;
+       nxt := tmp + nxt;
+   END LOOP;
+
+   RETURN ret;
+END;
+$$ LANGUAGE plpgsql;
+
+
+/*
+ * pts_steps
+ * Calculates steps from subsequent relocations
+ * Input: point geometry field
+ * Output: line geometry field
+ */
+CREATE OR REPLACE FUNCTION pts_steps (
+    pts geometry
+) RETURNS geometry AS $$
+BEGIN
+	
