05fdcac0d4865af0704bf85aadadc7a25cb2b8da
balazsdukai <balazs.dukai@gmail.com>
2016-05-28 12:10:03 +0200

    steps created, geom/geog length compared

--- pts_steps.sql
+++ pts_steps.sql
@@ -1,113 +1,68 @@
-/* Sample functions
- * reference: http://www.onlamp.com/pub/a/onlamp/2006/05/11/postgresql-plpgsql.html?page=2
-CREATE OR REPLACE FUNCTION fib (fib_for integer
-) RETURNS integer AS $$
-BEGIN
-IF fib_for < 2 THEN
-RETURN fib_for;
-END IF;
-RETURN fib(fib_for - 2) + fib(fib_for - 1);
-END;
-$$LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION upgis_utmzone_wgs84(geometry) 
+RETURNS integer AS
+$$ 
+DECLARE 
+    geomgeog geometry; 
+    zone int; 
+    pref int; 
+BEGIN 
+geomgeog:=ST_Transform(ST_Centroid($1),4326);                            --#1
+
+IF (st_y(geomgeog))>0 THEN                                                  --#2
+    pref:=32600;                                                         --#2
+ELSE                                                                     --#2
+    pref:=32700;                                                         --#2
+END IF;                                                                  --#2
+zone:=floor((ST_X(geomgeog)+180)/6)+1;                                   --#2
+
+RETURN zone+pref;
+END; 
+$$ LANGUAGE 'plpgsql' immutable; 
 
 
-CREATE OR REPLACE FUNCTION fib_fast(
-     fib_for integer
-) RETURNS integer AS $$
-DECLARE
-    ret integer := 0;
-    nxt integer := 1;
-    tmp integer;
-BEGIN
-   FOR num IN 1..fib_for LOOP
-        tmp := ret;
-       ret := nxt;
-       nxt := tmp + nxt;
-   END LOOP;
-
-   RETURN ret;
-END;
-$$ LANGUAGE plpgsql;
-*/
-
-/*
- * test function for learning PL/PgSQL
- */
-CREATE TABLE example_data.test(
-    id integer
-);
-INSERT INTO example_data.test(id) values(1);
-INSERT INTO example_data.test(id) values(2);
-INSERT INTO example_data.test(id) values(3);
-INSERT INTO example_data.test(id) values(4);
-INSERT INTO example_data.test(id) values(5);
-
-DROP FUNCTION test_function(text);
-
-CREATE OR REPLACE FUNCTION test_function (
-) RETURNS integer AS $$
-DECLARE
-    num record;
-    ret integer := 0;
-    counter integer;
-BEGIN
-	SELECT INTO counter count(id)
-	FROM example_data.test;
-	
-	WHILE counter - 1 > 0 LOOP
-    	RAISE NOTICE 'counter = %', counter;
-    	counter := counter - 1;
-    	RAISE NOTICE 'ret = %', ret;
-    	ret := ret + 1;
-	END LOOP;
-
-    RETURN ret;
-END
-$$ LANGUAGE plpgsql;
-
-SELECT test_function();
+DROP FUNCTION upgis_utmzone_wgs84(geometry);
 
 
--- table to store the steps with their length
+SELECT upgis_utmzone_wgs84(cont.geom)
+FROM (
+    SELECT geom
+    FROM example_data.continental
+) AS cont;
+
+-- table to store the steps
 CREATE TABLE example_data.steps (
-    gid integer PRIMARY KEY,
-    step geometry,
-    len float
+    startgid integer primary KEY,
+    id text,
+    step_geom geometry,
+    step_geog geography,
+    length_geom float4,
+    length_geog float4
 );
 
 
 /*
- * pts_steps
- * Calculates steps from subsequent relocations
- * Input: point geometry field
- * Output: line geometry field
+ * Creates steps
+ * Compares the length of the geometry and geography data types
  */
-CREATE OR REPLACE FUNCTION pts_steps (
-
-) RETURNS geometry AS $$
-DECLARE
-    line geometry;
-    dist float;
-BEGIN
-	FOR i IN example_data.continental.geom LOOP
-	INSERT INTO example_data.steps (
-	);
-END
-$$ LANGUAGE plpgsql;
-
-
-SELECT steps.startpt, step, st_length(st_transform(step, 3395)) AS geom_length
+SELECT 
+    steps.startgid,
+    step_geom,
+    step_geog,
+    st_length(st_transform(step_geom, 3395)) AS length_geom,
+    st_length(step_geog) AS length_geog
 FROM (
     SELECT 
-        a.gid AS startid,
+        a.gid AS startgid,
         b.gid,
-        st_makeline(a.geom, b.geom) AS step
+        st_makeline(a.geom, b.geom) AS step_geom,
+        st_makeline(a.geom, b.geom)::geography AS step_geog
     FROM 
         example_data.continental AS a
         INNER JOIN example_data.continental AS b ON a.gid + 1 = b.gid
     ORDER BY 
         a.gid
 ) AS steps;
+
 
 
 
