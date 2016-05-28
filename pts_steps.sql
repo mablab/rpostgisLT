bee3542f5ea4826f8f604d7aa731de9118eae540
balazsdukai <balazs.dukai@gmail.com>
2016-05-28 12:35:18 +0200

    steps table created

--- pts_steps.sql
+++ pts_steps.sql
@@ -30,7 +30,7 @@ FROM (
 ) AS cont;
 
 -- table to store the steps
-CREATE TABLE example_data.steps (
+CREATE TABLE IF NOT EXISTS example_data.steps (
     startgid integer primary KEY,
     id text,
     step_geom geometry,
@@ -44,8 +44,10 @@ CREATE TABLE example_data.steps (
  * Creates steps
  * Compares the length of the geometry and geography data types
  */
+INSERT INTO example_data.steps
 SELECT 
     steps.startgid,
+    steps.id,
     step_geom,
     step_geog,
     st_length(st_transform(step_geom, 3395)) AS length_geom,
@@ -54,15 +56,14 @@ FROM (
     SELECT 
         a.gid AS startgid,
         b.gid,
+        a.id,
         st_makeline(a.geom, b.geom) AS step_geom,
         st_makeline(a.geom, b.geom)::geography AS step_geog
     FROM 
-        example_data.continental AS a
-        INNER JOIN example_data.continental AS b ON a.gid + 1 = b.gid
+        example_data.relocations AS a
+        INNER JOIN example_data.relocations AS b ON a.gid + 1 = b.gid
     ORDER BY 
         a.gid
 ) AS steps;
-
-
 
 
