224c8c3aedfb20f7b520428f2c920dce442a36c8
balazsdukai <balazs.dukai@gmail.com>
2016-05-28 15:02:15 +0200

    create steps table

--- pts_steps.sql
+++ pts_steps.sql
@@ -1,33 +1,3 @@
-CREATE OR REPLACE FUNCTION upgis_utmzone_wgs84(geometry) 
-RETURNS integer AS
-$$ 
-DECLARE 
-    geomgeog geometry; 
-    zone int; 
-    pref int; 
-BEGIN 
-geomgeog:=ST_Transform(ST_Centroid($1),4326);                            --#1
-
-IF (st_y(geomgeog))>0 THEN                                                  --#2
-    pref:=32600;                                                         --#2
-ELSE                                                                     --#2
-    pref:=32700;                                                         --#2
-END IF;                                                                  --#2
-zone:=floor((ST_X(geomgeog)+180)/6)+1;                                   --#2
-
-RETURN zone+pref;
-END; 
-$$ LANGUAGE 'plpgsql' immutable; 
-
-
-DROP FUNCTION upgis_utmzone_wgs84(geometry);
-
-
-SELECT upgis_utmzone_wgs84(cont.geom)
-FROM (
-    SELECT geom
-    FROM example_data.continental
-) AS cont;
 
 -- table to store the steps
 CREATE TABLE IF NOT EXISTS example_data.steps (
@@ -65,5 +35,4 @@ FROM (
     ORDER BY 
         a.gid
 ) AS steps;
-
 
