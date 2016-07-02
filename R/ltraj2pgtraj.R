937e5715157126e16c10ea98e68458b8fc46fa26
balazsdukai <balazs.dukai@gmail.com>
2016-07-02 11:48:13 +0200

    renames files to R suffix instead of r

---
+++ R/ltraj2pgtraj.R
@@ -0,0 +1,9 @@
+
+
+# Convert ltraj to data frame
+df <- ld(ltraj)
+
+# Create reloc_temp
+
+
+
---
+++ tests/t_as_pgtraj.R
@@ -0,0 +1,42 @@
+# 
+# Author: bdukai
+###############################################################################
+library(RPostgreSQL)
+library(rpostgis)
+
+source("./R/as_pgtraj.r")
+
+# all variables stored with the raw data
+as_pgtraj(conn, 
+        schema = "traj_t1",
+        relocation_data = "example_data.relocations_plus", 
+        pgtrajs = "id",
+        animals = "animal",
+        bursts = "burst",
+        relocations = "geom",
+        timestamp = "time",
+        rid = "gid")
+
+# variables provided manually
+as_pgtraj(conn, 
+        schema = "traj_t2",
+        relocation_data = "example_data.reloc_medium", 
+        pgtrajs = "medium",
+        animals = "sea turtle",
+        relocations = "geom",
+        timestamp = "time",
+        rid = "gid")
+
+# trajectory Type I
+as_pgtraj(conn, 
+        schema = "traj_t3",
+        relocation_data = "example_data.reloc_t1", 
+        pgtrajs = "small",
+        animals = "small animal",
+        relocations = "geom",
+        rid = "gid")
+
+dbGetQuery(conn, "set search_path to traj_t3, public;")
+suppressWarnings(dbGetQuery(conn, "select step from steps limit 1;"))
+
+t <- c(t, dbGetQuery(conn, "select step from steps limit 1;"))
