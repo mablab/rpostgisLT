cd720a7736794c6d5f93fc3f9ab5aceeca5f3f53
balazsdukai <balazs.dukai@gmail.com>
2016-07-02 17:45:55 +0200

    make_reloc_temp

---
+++ R/make_reloc_temp.R
@@ -0,0 +1,20 @@
+#' Creates the table 'reloc_temp', which is a temporary table used by other
+#' function of the package.
+#' 
+#' @param conn Connection object created with RPostgreSQL
+#' @param schema String. Name of the schema that stores or will store the pgtraj data model.
+#' 
+###############################################################################
+make_reloc_temp <- function(conn, schema) {
+    # Create 'reloc_temp' table
+    query <- paste0("CREATE TABLE ", schema, ".reloc_temp (
+                    r_id    integer,
+                    relocation     geometry,
+                    date    timestamptz,
+                    b_name      text,
+                    a_name      text,
+                    p_name      text
+                    );")
+    query <- gsub(pattern = '\\s', replacement = " ", x = query)
+    dbGetQuery(conn, query)
+}
