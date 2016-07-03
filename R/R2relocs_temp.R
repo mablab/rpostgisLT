a170946d5218fd1f7757643b491e2ba23197541d
balazsdukai <balazs.dukai@gmail.com>
2016-07-03 23:30:12 +0200

    ltraj inserted into relocs_temp

--- R/R2relocs_temp.R
+++ R/R2relocs_temp.R
@@ -3,29 +3,17 @@
 #' The function does not preserve the row names of the ltraj, thus once relocations
 #' are imported into the database, they cannot be linked back to the relocations 
 #' in the original.
-#' Missing values are ommitted in the database import, because
+#' Missing relocations are ignored, because they cannot be cast into geometry
+#' in the database.
 #' 
 ###############################################################################
-
-library(sp)
-
-data(ibex)
-dframe <- ld_opt(ibex)
-pgtraj <-  "ibex"
-schema <- "traj_t4" 
-epsg <- 4326
-epsg <- 0
-epgs <- NULL
-
 R2relocs_temp <- function(conn, schema, dframe, pgtraj, epsg = NULL) {
     # Set projection string if 
     if (epsg %% 1 == 0 & epsg > 0) {
         proj <- CRS(paste0("+init=epsg:", epsg))
     }
-    # Missing relocations are ignored, because they cannot be cast into geometry
     DF <- dframe[complete.cases(dframe[,c("x", "y")]),]
     # Prepare the data frame to match 'relocs_temp'
-    #DF <- cbind(DF, "r_id" = row.names(DF))
     coords <- SpatialPoints(DF[, c("x", "y")], proj4string = proj)
     spdf <- SpatialPointsDataFrame(coords, DF)
     spdf <- spdf[,c("date", "id", "burst")]
--- R/ltraj2pgtraj.R
+++ R/ltraj2pgtraj.R
@@ -1,9 +1,25 @@
 
+# TODO once SRID is stored in the ltraj, include that too
 
-# Convert ltraj to data frame
-df <- ld(ltraj)
+ltraj2pgtraj <- function(ltraj, conn, schema, pgtraj = NULL, epsg = NULL) {
+    # 'pgtraj' defaults to the name of ltraj
+    if (is.null(pgtraj)) {
+        pgtraj <- deparse(substitute(ltraj))
+    }
+    
+    # Convert ltraj to data frame
+    dframe <- ld_opt(ltraj)
+    
+    # Import data frame into a temporary table
+    make_relocs_temp(conn, schema)
+    R2relocs_temp(conn, schema, dframe, pgtraj, epsg)
+    
+    # TODO Insert ltraj into the schema
+    
+    # Drop temporary table
+    drop_relocs_temp(conn, schema)
+}
 
-# Create reloc_temp
-
+#records <- ltraj2pgtraj(ibex)
 
 
