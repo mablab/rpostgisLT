8edfef31488b9630f60634f1eaf284a1bec4f298
balazsdukai <balazs.dukai@gmail.com>
2016-07-18 16:54:40 +0200

    update test with DB connection

--- tests/tests.R
+++ tests/tests.R
@@ -1,3 +1,7 @@
+## Establish connection with rpostgisLT database
+source("./rpostgisLT/utility_functions.R")
+cs() # creates globals conn and drv
+
 ## Basic ltraj
 library(adehabitatLT)
 data(ibexraw)
@@ -7,22 +11,23 @@ is.regular(ibexraw)
 
 ## Note that there is an issue with the time zone. In 'ibexraw', the
 ## time zone is not set:
-attr(ld(ibexraw)$date, "tzone")
+attr(ld_opt(ibexraw)$date, "tzone")
 ## This means that it is assumed to be UTC, and is thus converted to
 ## local time zone on display (EDT2EST for me):
-head(ld(ibexraw)$date)                 # Note that the first timestamp
+head(ld_opt(ibexraw)$date)                 # Note that the first timestamp
                                        # should be '2003-06-01
                                        # 00:00:56'
 ## We need to fix that upfront:
-ibex <- ld(ibexraw)
+ibex <- ld_opt(ibexraw)
 attr(ibex$date, "tzone") <- "Europe/Paris"
-ibex <- dl(ibex)
+ibex <- dl_opt(ibex)
 
-ltraj2pgtraj(ibex)                   # Default should be in schema
+ltraj2pgtraj(conn, ibex)                   # Default should be in schema
                                         # 'traj' and use ltraj name
                                         # ('ibexraw') as pgtraj name.
-ibexTest <- pgtraj2ltraj("ibex")     # Default should look into
+ibexTest <- pgtraj2ltraj(conn, pgtraj = "ibex")     # Default should look into
                                         # 'traj' schema.
+all.equal(ibex, ibexTest)
 identical(ibex, ibexTest)
 ## TRUE
 
@@ -31,8 +36,8 @@ identical(ibex, ibexTest)
 refda <- strptime("2003-06-01 00:00", "%Y-%m-%d %H:%M",
     tz = "Europe/Paris")
 (ibex <- setNA(ibex, refda, 4, units = "hour"))
-ltraj2pgtraj(ibex, overwrite = TRUE)
-ibexTest <- pgtraj2ltraj("ibex")
+ltraj2pgtraj(conn, ibex, overwrite = TRUE)
+ibexTest <- pgtraj2ltraj(conn, pgtraj = "ibex")
 identical(ibex, ibexTest)
 ## TRUE
 
@@ -41,8 +46,8 @@ identical(ibex, ibexTest)
 (ibex <- sett0(ibex, refda, 4, units = "hour"))
 ibex.ref <- ibex                        # At this stage, 'ibex' is our
                                         # reference data
-ltraj2pgtraj(ibex, overwrite = TRUE)
-ibexTest <- pgtraj2ltraj("ibex")
+ltraj2pgtraj(conn, ibex, overwrite = TRUE)
+ibexTest <- pgtraj2ltraj(conn, pgtraj = "ibex")
 identical(ibex, ibexTest)
 ## TRUE
 
@@ -50,14 +55,14 @@ identical(ibex, ibexTest)
 ## Interpolation
 
 ## 1. In space
-summary(ld(ibex)$dist)
+summary(ld_opt(ibex)$dist)
 (ibex <- redisltraj(ibex, 400))         # Note that 'redisltraj'
                                         # creates an 'infolocs'
                                         # attribute, which we remove
                                         # for now:
 ibex <- removeinfo(ibex)
-ltraj2pgtraj(ibex, overwrite = TRUE)
-ibexTest <- pgtraj2ltraj("ibex")
+ltraj2pgtraj(conn, ibex, overwrite = TRUE)
+ibexTest <- pgtraj2ltraj(conn, pgtraj = "ibex")
 identical(ibex, ibexTest)
 ## TRUE
 
@@ -65,8 +70,8 @@ identical(ibex, ibexTest)
 ibex <- ibex.ref
 (ibex <- redisltraj(na.omit(ibex), 14400, type = "time"))
 ibex <- removeinfo(ibex)
-ltraj2pgtraj(ibex, overwrite = TRUE)
-ibexTest <- pgtraj2ltraj("ibex")
+ltraj2pgtraj(conn, ibex, overwrite = TRUE)
+ibexTest <- pgtraj2ltraj(conn, pgtraj = "ibex")
 identical(ibex, ibexTest)
 ## TRUE
 
@@ -79,34 +84,19 @@ ibex <- ibex.ref
 ## then rebuild the ltraj without recomputing trajectory parameters;
 ## this is essentially what 'hab::subset' does.
 ## Note that the steps are not discontinuous any more.
-ibex <- ld(ibex)
+ibex <- ld_opt(ibex)
 ibex <- droplevels(ibex[ibex$dist < 400 & !is.na(ibex$dist), ])
-dlfast <- function(x) {
-    trajnam <- c("x", "y", "date", "dx", "dy", "dist", "dt",
-        "R2n", "abs.angle", "rel.angle")
-    idd <- tapply(as.character(x$id), x$burst, unique)
-    traj <- split(x[, names(x) %in% trajnam], x$burst)
-    names(traj) <- NULL
-    class(traj) <- c("ltraj", "list")
-    attr(traj, "typeII") <- TRUE
-    attr(traj, "regular") <- is.regular(traj)
-    for (i in (1:length(traj))) {
-        attr(traj[[i]], "id") <- as.character(idd[i])
-        attr(traj[[i]], "burst") <- names(idd[i])
-    }
-    return(traj)
-}
-ibex <- dlfast(ibex)
+ibex <- dl_opt(ibex)
 head(ibex[[1]])
-ltraj2pgtraj(ibex, overwrite = TRUE)
-ibexTest <- pgtraj2ltraj("ibex")
+ltraj2pgtraj(conn, ibex, overwrite = TRUE)
+ibexTest <- pgtraj2ltraj(conn, pgtraj = "ibex")
 identical(ibex, ibexTest)
 
 ## 2. Subsample on the temporal sequence
 ibex <- ibex.ref
 (ibex <- subsample(ibex, 14400*2))
-ltraj2pgtraj(ibex, overwrite = TRUE)
-ibexTest <- pgtraj2ltraj("ibex")
+ltraj2pgtraj(conn, ibex, overwrite = TRUE)
+ibexTest <- pgtraj2ltraj(conn, pgtraj = "ibex")
 identical(ibex, ibexTest)
 
 
@@ -115,15 +105,15 @@ identical(ibex, ibexTest)
 ## 1. Cut if there is a step greater than 3000 m
 ibex <- ibex.ref
 (ibex <- cutltraj(ibex, "dist > 3000"))
-ltraj2pgtraj(ibex, overwrite = TRUE)
-ibexTest <- pgtraj2ltraj("ibex")
+ltraj2pgtraj(conn, ibex, overwrite = TRUE)
+ibexTest <- pgtraj2ltraj(conn, pgtraj = "ibex")
 identical(ibex, ibexTest)
 
 ## 2. Bind back by individual:
 (ibex <- bindltraj(ibex))
 ibex <- removeinfo(ibex)
-ltraj2pgtraj(ibex, overwrite = TRUE)
-ibexTest <- pgtraj2ltraj("ibex")
+ltraj2pgtraj(conn, ibex, overwrite = TRUE)
+ibexTest <- pgtraj2ltraj(conn, pgtraj = "ibex")
 identical(ibex, ibexTest)
 
 
@@ -132,6 +122,6 @@ ibex <- ibex.ref
 ibex2 <- ibex
 burst(ibex2) <- paste(burst(ibex2), "2", sep = "-")
 (ibex <- c(ibex, ibex2)[order(id(c(ibex, ibex2)))])
-ltraj2pgtraj(ibex, overwrite = TRUE)
-ibexTest <- pgtraj2ltraj("ibex")
+ltraj2pgtraj(conn, ibex, overwrite = TRUE)
+ibexTest <- pgtraj2ltraj(conn, pgtraj = "ibex")
 identical(ibex, ibexTest)
