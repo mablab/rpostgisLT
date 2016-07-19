d14c7b7f27192b49b7c204a83bea2737e2951a01
balazsdukai <balazs.dukai@gmail.com>
2016-07-19 21:07:57 +0200

    fix step parameters
    fix R2n

--- R/pgTrajParamsView.R
+++ R/pgTrajParamsView.R
@@ -36,50 +36,50 @@ pgTrajParamsView <- function(conn, schema, pgtraj, epsg) {
     t.pgtraj
     FROM 
         (
-            SELECT 
-            s.r_rowname,
-            ST_x(s.reloc1) AS x, 
-            ST_y(s.reloc1) AS y,
-            s.date,
-            ST_X(ST_endpoint(s.step)) - ST_X(ST_startpoint(s.step)) AS dx,
-            ST_Y(ST_endpoint(s.step)) - ST_Y(ST_startpoint(s.step)) AS dy,
-            ST_length(s.step) AS dist,
-            extract(epoch FROM s.dt) AS dt,
-            ST_Distance(startp.reloc1, s.reloc1) AS r2n,
+        SELECT 
+        s.r_rowname,
+        ST_x(s.reloc1) AS x, 
+        ST_y(s.reloc1) AS y,
+        s.date,
+        ST_X(ST_endpoint(s.step)) - ST_X(ST_startpoint(s.step)) AS dx,
+        ST_Y(ST_endpoint(s.step)) - ST_Y(ST_startpoint(s.step)) AS dy,
+        ST_length(s.step) AS dist,
+        extract(epoch FROM s.dt) AS dt,
+        ST_Distance(startp.reloc1, s.reloc1)^2 AS r2n,
+        (
+        ST_Azimuth(ST_startpoint(s2.step), ST_endpoint(s2.step)) -
+        ST_Azimuth(ST_startpoint(s.step), ST_endpoint(s.step))
+        ) AS rel_angle,
+        a.a_name AS id,
+        b.b_name AS burst,
+        p.p_name AS pgtraj
+        FROM steps AS s 
+        LEFT JOIN steps AS s2 ON s.s_id + 1 = s2.s_id
+        JOIN s_i_b_rel AS s_rel ON s.s_id = s_rel.s_id
+        JOIN bursts AS b ON s_rel.b_id = b.b_id
+        JOIN animals AS a ON b.a_id = a.a_id
+        JOIN p_b_rel AS p_rel ON p_rel.b_id = b.b_id
+        JOIN pgtrajs AS p ON p_rel.p_id = p.p_id
+        JOIN 
+        (
+        SELECT 
+        m.*,
+        s.reloc1
+        FROM 
             (
-            ST_Azimuth(ST_startpoint(s2.step), ST_endpoint(s2.step)) -
-            ST_Azimuth(ST_startpoint(s.step), ST_endpoint(s.step))
-            ) AS rel_angle,
-            a.a_name AS id,
-            b.b_name AS burst,
-            p.p_name AS pgtraj
-            FROM steps AS s 
-            LEFT JOIN steps AS s2 ON s.s_id + 1 = s2.s_id
+            SELECT
+            min(s.s_id) AS s_id,
+            s_rel.b_id
+            FROM steps AS s
             JOIN s_i_b_rel AS s_rel ON s.s_id = s_rel.s_id
-            JOIN bursts AS b ON s_rel.b_id = b.b_id
-            JOIN animals AS a ON b.a_id = a.a_id
-            JOIN p_b_rel AS p_rel ON p_rel.b_id = b.b_id
+            JOIN p_b_rel AS p_rel ON p_rel.b_id = s_rel.b_id
             JOIN pgtrajs AS p ON p_rel.p_id = p.p_id
-            JOIN 
-            (
-            SELECT 
-            m.*,
-            s.reloc1
-            FROM 
-                (
-                SELECT
-                min(s.s_id) AS s_id,
-                s_rel.b_id
-                FROM steps AS s
-                JOIN s_i_b_rel AS s_rel ON s.s_id = s_rel.s_id
-                JOIN p_b_rel AS p_rel ON p_rel.b_id = s_rel.b_id
-                JOIN pgtrajs AS p ON p_rel.p_id = p.p_id
-                WHERE p_name LIKE '",pgtraj,"'
-                GROUP BY s_rel.b_id
-                ) AS m
-            JOIN steps AS s ON s.s_id = m.s_id
-            ) AS startp ON startp.b_id = s_rel.b_id
             WHERE p_name LIKE '",pgtraj,"'
+            GROUP BY s_rel.b_id
+            ) AS m
+        JOIN steps AS s ON s.s_id = m.s_id
+        ) AS startp ON startp.b_id = s_rel.b_id
+        WHERE p_name LIKE '",pgtraj,"'
     ) AS t;")
     query <- gsub(pattern = '\\s', replacement = " ", x = query)
     invisible(RPostgreSQL::dbGetQuery(conn, query))
