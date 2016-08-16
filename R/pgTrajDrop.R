d59429a89e3fcb40a862aab473da11aaef9643d3
Balázs Dukai <balazs.dukai@gmail.com>
2016-08-16 19:05:54 +0200

    untested

--- R/pgTrajDrop.R
+++ R/pgTrajDrop.R
@@ -1,7 +1,7 @@
 #' Delete one or more pgtrajs from a traj schema.
 #' 
 #' @description 
-#' \code{pgTrajDelete} deletes one or more pgtrajs from a traj schema.
+#' \code{pgTrajDrop} deletes one or more pgtrajs from a traj schema.
 #' 
 #' @author Balázs Dukai \email{balazs.dukai@@gmail.com}
 #' 
@@ -9,16 +9,80 @@
 #' @param schema String. Name of the schema that stores the traj data model.
 #' @param pgtraj String. A vector containing the names of the pgtrajs.
 #' 
-#' @return nothing
+#' @return TRUE on success
 #' 
 #' @examples 
-#' \dontrun{pgTrajDelete(conn, "traj", "ibex")}
+#' \dontrun{pgTrajDrop(conn, "traj", "ibex")}
 #' 
-#' \dontrun{pgTrajDelete(conn, schema="traj", pgtraj=c("ibex", "puechcirc")}
+#' \dontrun{pgTrajDrop(conn, schema="traj", pgtraj=c("ibex", "puechcirc")}
 #' 
 #' @export 
 #' 
 
-pgTrajDelete <- function(conn, schema = "traj", pgtraj) {
-    query <- paste0("")
+pgTrajDrop <- function(conn, schema = "traj", pgtraj) {
+    
+    # Set database search path
+    current_search_path <- dbGetQuery(conn, "SHOW search_path;")
+    query <- paste0("SET search_path TO ", schema, ",public;")
+    invisible(dbGetQuery(conn, query))
+    
+    query <- paste0("
+                    DELETE FROM infoloc WHERE id IN (
+                        SELECT
+                            i.id AS infoloc_id
+                        FROM infoloc i
+                        JOIN s_i_b_rel rel ON rel.infoloc_id = i.id
+                        JOIN animal_burst ab ON ab.id = rel.animal_burst_id
+                        JOIN pgtraj p ON p.id = ab.pgtraj_id
+                        WHERE p.pgtraj_name = '",pgtraj,"'
+                        );
+                    
+                    DELETE FROM relocation WHERE id IN (
+                        SELECT
+                            r1.id AS relocation_id
+                        FROM step s
+                        JOIN relocation r1 ON s.relocation_id_1 = r1.id
+                        LEFT JOIN relocation r2 ON s.relocation_id_2 = r2.id
+                        JOIN s_i_b_rel rel ON rel.step_id = s.id
+                        JOIN animal_burst ab ON ab.id = rel.animal_burst_id
+                        JOIN pgtraj p ON p.id = ab.pgtraj_id
+                        WHERE p.pgtraj_name = '",pgtraj,"'
+                        );
+                        
+                    DELETE FROM pgtraj WHERE pgtraj_name = '",pgtraj,"';
+                    ")
+    query <- gsub(pattern = '\\s', replacement = " ", x = query)
+    
+    # Begin transaction block
+    invisible(dbSendQuery(conn, "BEGIN TRANSACTION;"))
+    
+    tryCatch({
+            
+            invisible(dbSendQuery(conn, query))
+            dbCommit(conn)
+        
+        }, warning = function(x) {
+            
+            message(x)
+            message(". Rolling back transaction")
+            dbRollback(conn)
+            stop("Returning from function")
+            
+        }, error = function(x) {
+            
+            message(x)
+            message(". Rolling back transaction")
+            dbRollback(conn)
+            stop("Returning from function")
+            
+        })
+
+    # Restore database search path
+    query <- paste0("SET search_path TO ", current_search_path, ";")
+    invisible(dbSendQuery(conn, query))
+    
+    message(paste0("The pgtraj '", pgtraj, "' deleted from the database schema '", schema,"'."))
+    return(TRUE)
+
 }
+
