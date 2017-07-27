11dbafa3c9848d9e1e17883fc25e09a082369efc
Balázs Dukai <balazs.dukai@gmail.com>
2017-07-27 11:13:39 +0200

    fix field quoting in infolocs tables; fix 0 SRID error; add error handling

--- shiny/createShinyView.R
+++ shiny/createShinyView.R
@@ -22,30 +22,39 @@ createShinyView <- function(conn, schema, pgtraj) {
                         dbQuoteIdentifier(conn, schema), ",public;")
     invisible(dbExecute(conn, sql_query))
     
+    pgtraj_s <- dbQuoteString(conn, pgtraj)
     view <- dbQuoteIdentifier(conn, paste0("step_geometry_shiny_",pgtraj))
     
     infolocs_table <- paste0("infolocs_", pgtraj)
     info_cols <- getInfolocsColumns(conn, schema, infolocs_table)
     
     # if there is an infolocs table
-    if(nrow(info_cols) > 0) {
-        cols <- paste(paste(paste0("i.",
-                                   info_cols$column_name
-        ),
-        collapse = ", "
-        ),
-        ",")
-        join <- paste0("JOIN ", infolocs_table," i ON p.step_id = i.step_id")
+    if (nrow(info_cols) > 0) {
+        cols <- paste(paste0("i.",
+                             dbQuoteIdentifier(conn, info_cols$column_name)),
+                      collapse = ", ")
+        join <-
+            paste0("JOIN ", infolocs_table, " i ON p.step_id = i.step_id")
     } else {
         cols <- NULL
         join <- NULL
+    }
+    
+    # In case the relocations are not projected
+    sql_query <- paste0("SELECT proj4string FROM pgtraj WHERE pgtraj_name = ", pgtraj_s,";")
+    srid <- dbGetQuery(conn, sql_query)$proj4string
+    
+    if(is.na(srid)) {
+        step_geom <- "st_makeline(r1.geom, r2.geom) AS step_geom"
+    } else {
+        step_geom <- "st_transform(st_makeline(r1.geom, r2.geom), 4326)::geometry(LineString,4326) AS step_geom"
     }
     
     sql_query <- paste0("
         CREATE MATERIALIZED VIEW IF NOT EXISTS ", view, " AS
             SELECT
                 p.step_id,
-                st_transform(st_makeline(r1.geom, r2.geom), 4326)::geometry(LineString,4326) AS step_geom,
+                ",step_geom,",
                 r1.relocation_time AS date,
                 p.dx,
                 p.dy,
@@ -53,7 +62,7 @@ createShinyView <- function(conn, schema, pgtraj) {
                 p.dt,
                 p.abs_angle,
                 p.rel_angle,
-                ",cols,"
+                ",cols,",
                 p.animal_name,
                 p.burst AS burst_name,
                 p.pgtraj AS pgtraj_name
@@ -76,8 +85,37 @@ createShinyView <- function(conn, schema, pgtraj) {
     
     create_sql_query <- gsub(pattern = '\\s', replacement = " ",
                              x = sql_query)
-    invisible(dbExecute(conn, create_sql_query))
-    message(paste0("View 'step_geometry_shiny_",pgtraj,"' created in schema '",
+    
+    tryCatch({
+        invisible(dbExecute(conn, create_sql_query))
+        TRUE
+    }, warning = function(x) {
+        message(x)
+        message(" . Cannot CREATE MATERIALIZED VIEW")
+        ## Restore database search path
+        sql_query <- paste0("SET search_path TO ", current_search_path, ";")
+        invisible(dbExecute(conn, sql_query))
+        stop("Returning from function")
+        
+    }, error = function(x) {
+        message(x)
+        message(". Cannot CREATE MATERIALIZED VIEW")
+        ## Restore database search path
+        sql_query <- paste0("SET search_path TO ", current_search_path, ";")
+        invisible(dbExecute(conn, sql_query))
+        stop("Returning from function")
+    })
+    
+    # tryCatch(if(invisible(dbExecute(conn, create_sql_query))),
+    #          error = function() {
+    #              sql_query <- paste0("SET search_path TO ", current_search_path, ";")
+    #              invisible(dbExecute(conn, sql_query))
+    #              
+    #              stop()
+    #                 
+    #          })
+    
+    message(paste0("MATERIALIZED VIEW 'step_geometry_shiny_",pgtraj,"' created in schema '",
                    schema, "'."))
     
     dbVacuum(conn, name = paste0("step_geometry_shiny_",pgtraj), analyze = TRUE)
