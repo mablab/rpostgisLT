538f1536360989d40dd92023401f6b4c3bb5e1fd
Balázs Dukai <balazs.dukai@gmail.com>
2017-08-22 14:23:31 +0200

    update test data

---
+++ inst/extdata/restore-rpglt_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# usage: ./restore-rpglt_data.sh <name of the DB>
+
+dropdb $1
+createdb $1
+psql -d $1 -c "create extension postgis"
+psql -d $1 -c "create schema example_data; create schema ibex_traj_materialized_bursts;"
+gzip -dc ./rpglt_data.sql.gz | psql -d $1
