f68a689005edb2d33d189e4838fc1b45ba7819e1
Balázs Dukai <balazs.dukai@gmail.com>
2017-08-22 17:24:00 +0200

    update dataset

--- inst/extdata/backup-rpglt_data.sh
+++ inst/extdata/backup-rpglt_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-pg_dump --host localhost --port 5432 --username "bdukai" --no-password  --format plain --no-owner --encoding UTF8 --no-privileges --verbose --quote-all-identifiers --file "./rpglt_data.sql" --table "example_data.reloc_medium" --table "example_data.reloc_t1" --table "example_data.relocations_plus" --table "example_data.test_line" --table "example_data.test_points" --table "example_data.test_polygons" --table "public.infoloc_test" --schema "ibex_traj_materialized_bursts" "rpglt_data"
+pg_dump --host localhost --port 5432 --username "bdukai" --no-password  --format plain --no-owner --encoding UTF8 --no-privileges --verbose --quote-all-identifiers --file "./inst/extdata/rpglt_data.sql" --table "example_data.reloc_medium" --table "example_data.reloc_t1" --table "example_data.relocations_plus" --table "example_data.test_line" --table "example_data.test_points" --table "example_data.test_polygons" --table "public.infoloc_test" --schema "ibex_traj_materialized_bursts" "rpglt_data"
 
-gzip -f ./rpglt_data.sql
+gzip -f ./inst/extdata/rpglt_data.sql
 
 
