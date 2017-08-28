25dffa0a94e540d80dcba2d1ae7e7d5ebff8e592
Balázs Dukai <balazs.dukai@gmail.com>
2017-08-28 17:30:45 +0200

    update data sets; CI builds

--- inst/extdata/backup-rpglt_data.sh
+++ inst/extdata/backup-rpglt_data.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-pg_dump --host localhost --port 5432 --username "bdukai" --no-password  --format plain --no-owner --encoding UTF8 --no-privileges --verbose --quote-all-identifiers --file "./inst/extdata/rpglt_data.sql" --table "example_data.reloc_medium" --table "example_data.reloc_t1" --table "example_data.relocations_plus" --table "example_data.test_line" --table "example_data.test_points" --table "example_data.test_polygons" --table "public.infoloc_test" --schema "ibex_traj_materialized_bursts" "rpglt_data"
+/usr/bin/pg_dump --host localhost --port 5432 --username "bdukai" --no-password  --format plain --section pre-data --section data --section post-data --encoding UTF8 --no-privileges --verbose --quote-all-identifiers --file "rpglt_data.sql" --table "example_data.albatross" --table "example_data.ibex" --table "example_data.reloc_medium" --table "example_data.reloc_t1" --table "example_data.relocations_plus" --table "example_data.test_line" --table "example_data.test_points_mixed" --table "example_data.test_points_multi" --table "example_data.test_points_multi_3395" --table "example_data.test_polygons" --table "public.infoloc_test" "rpglt_data"
 
-gzip -f ./inst/extdata/rpglt_data.sql
+/usr/bin/pg_dump --host localhost --port 5432 --username "bdukai" --no-password  --format plain --no-owner --section pre-data --section data --section post-data --encoding UTF8 --no-privileges --verbose --quote-all-identifiers --file "ibex_traj_materialized_bursts.sql" --schema "ibex_traj_materialized_bursts" "rpglt_data"
 
+gzip -f rpglt_data.sql
+gzip -f ibex_traj_materialized_bursts.sql
 
